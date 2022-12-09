80911eb54662f5a60de076d829c2e796c06da0c4
Sekai <ahmedx793@gmail.com>
2022-12-09 02:42:33 +0200

    no meili!!!!!

--- src/meili.service.ts
+++ src/meili.service.ts
@@ -10,19 +10,5 @@ export class MeiliService extends MeiliSearch {
       apiKey: 'admin1234',
     });
   }
-  async onAppInit() {
-    console.log('[MEILI INIT]: Updating Index Settings');
 
-    await this.index('manga').updateSettings({
-      searchableAttributes: ['title.*', 'title.en', 'altTitles'],
-    });
-
-    console.log('[MEILI INIT]: Getting manga');
-    const allManga = await this.prisma.manga.findMany({});
-    console.log('[MEILI INIT]: Got manga');
-    const mangaIndex = this.index('manga');
-    console.log('[MEILI INIT]: Importing manga');
-    const task = await mangaIndex.addDocuments(allManga, { primaryKey: 'id' });
-    console.log(`[MEILI INIT]: Imported manga`);
-  }
 }
