bd98e6c504fa52121244ee26ca95eb3e51a3c1bd
Sekai <ahmedx793@gmail.com>
2022-12-25 19:04:18 +0200

    pls work gc

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MeiliService } from './meili.service';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT, () => console.log(PORT));
+
+  const meiliService = app.get(MeiliService);
+
+  meiliService.onAppInit();
 }
 bootstrap();
--- src/meili.service.ts
+++ src/meili.service.ts
@@ -6,9 +6,26 @@ import { PrismaService } from './prisma.service';
 export class MeiliService extends MeiliSearch {
   constructor(private prisma: PrismaService) {
     super({
-      host: 'https://meilisearch-production-d30d.up.railway.app',
+      host: 'http://13.36.37.93:7700/',
       apiKey: 'admin1234',
     });
   }
 
+  async onAppInit() {
+    console.log('[MEILI INIT]: Updating Index Settings');
+
+    await this.index('manga').updateSettings({
+      searchableAttributes: ['title.*', 'title.en', 'altTitles'],
+    });
+
+    const mangaIndex = this.index('manga');
+    
+    console.log('[MEILI INIT]: Importing manga');
+
+    await mangaIndex.addDocuments(await this.prisma.manga.findMany({}), {
+      primaryKey: 'id',
+    });
+
+    console.log(`[MEILI INIT]: Imported manga`);
+  }
 }
